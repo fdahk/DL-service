404caf27064d3537e2d92834b0f2b63db2cc8f4c
Jiang Tu <3235159187@qq.com>
2025-07-14 18:05:05 +0800

    feat: init

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginVue from "eslint-plugin-vue";
+import { defineConfig } from "eslint/config";
+
+// 导出 ESLint 配置数组，供 ESLint 使用
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,vue}"], plugins: { js }, extends: ["js/recommended"] },
+  // 添加 wx 全局变量
+  { files: ["**/*.{js,mjs,cjs,vue}"], languageOptions: { 
+     globals: { ...globals.browser, wx: "readonly" } 
+   } },
+  pluginVue.configs["flat/essential"],
+]);
